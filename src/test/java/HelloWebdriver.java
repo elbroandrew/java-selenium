0209485f82152b67e142ec8b60339f28cd595e86
Andrew <elbroandrew@gmail.com>
2021-12-22 17:37:00 +1000

    feat: add first xpath

--- src/test/java/HelloWebdriver.java
+++ src/test/java/HelloWebdriver.java
@@ -1,10 +1,16 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HelloWebdriver {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("http://hh.ru");
+        WebElement searchInput = driver.findElement(By.xpath("//input[contains(@class, 'bloko-input')]"));
+        searchInput.sendKeys("QA Java");
+        WebElement searchBtn = driver.findElement(By.xpath("//button[contains(@class, 'bloko-button_scale-large')]"));
+        searchBtn.click();
         Thread.sleep(2000);
         driver.quit();
     }
