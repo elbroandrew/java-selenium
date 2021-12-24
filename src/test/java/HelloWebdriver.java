8a12beeb3fad4fe18462df199f0449c76ac5589b
Andrew <elbroandrew@gmail.com>
2021-12-24 10:59:46 +1000

    feat: add timeouts

--- src/test/java/HelloWebdriver.java
+++ src/test/java/HelloWebdriver.java
@@ -3,9 +3,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class HelloWebdriver {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         driver.get("http://hh.ru");
         WebElement searchInput = driver.findElement(By.xpath("//input[contains(@class, 'bloko-input')]"));
         searchInput.sendKeys("QA Java");
