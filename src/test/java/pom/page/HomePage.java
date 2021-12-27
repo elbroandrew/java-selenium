e269c9ca81cb328b87081f3d03433c20b22daca2
Andrew <elbroandrew@gmail.com>
2021-12-27 12:13:38 +1000

    refactor: почему cachelookup не нужон

--- src/test/java/pom/page/HomePage.java
+++ src/test/java/pom/page/HomePage.java
@@ -3,6 +3,7 @@ package pom.page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,6 +18,8 @@ public class HomePage extends AbstractPAge {
     private WebElement searchInput;
 
     @FindBy(xpath = "//button[contains(@class, 'l-page-title__form-submit')]")
+    //@CacheLookup - в древние времена было все статично - было хорошо, сейчас динамично - не надо cache lookup использовать, иначе, если элемента
+    //нет, то выдаст stale element и тд, т.е. проиграем в стабильности, выиграв в скорости.
     private WebElement searchButton;
 
     public HomePage(WebDriver driver) {
