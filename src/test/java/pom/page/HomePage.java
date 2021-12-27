01f86949aef7ff318d4292f1757b830aebc521b2
Andrew <elbroandrew@gmail.com>
2021-12-27 10:24:25 +1000

    feat: pom home page

--- src/test/java/pom/page/HomePage.java
+++ src/test/java/pom/page/HomePage.java
@@ -1,20 +1,46 @@
 package pom.page;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import waits.CustomConditions;
 
 import java.time.Duration;
 
 public class HomePage {
-        driver.get("https://career.habr.com/");
+    private static final String HOMEPAGE_URL = "https://career.habr.com/";
+    private WebDriver driver;
 
+    @FindBy(className = "l-page-title__input")
+    private WebElement searchInput;
+
+    @FindBy(xpath = "//button[contains(@class, 'l-page-title__form-submit')]")
+    private WebElement searchButton;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    };
+    public HomePage openPage(){
+        driver.get(HOMEPAGE_URL);
         new WebDriverWait(driver, Duration.ofSeconds(5))
-            .until(CustomConditions.jQueryAJAXCompleted());
+                .until(CustomConditions.jQueryAJAXCompleted());
+        return this;
+    };
 
-    WebElement searchInput = waitForElementLocatedBy(driver, By.className("l-page-title__input"));
+    public SearchResultsPage searchForTerms(String term){
         searchInput.sendKeys("QA Java");
+        searchButton.click();
+        return new SearchResultsPage(driver, term);
+    }
 
-    WebElement searchBtn = driver.findElement(By.xpath("//button[contains(@class, 'l-page-title__form-submit')]"));
-        searchBtn.click();
+//
+//    WebElement searchInput = waitForElementLocatedBy(driver, By.className("l-page-title__input"));
+//        searchInput.sendKeys("QA Java");
+//
+//    WebElement searchBtn = driver.findElement(By.xpath("//button[contains(@class, 'l-page-title__form-submit')]"));
+//        searchBtn.click();
 }
--- src/test/java/pom/test/WebDriverSeleniumTest.java
+++ src/test/java/pom/test/WebDriverSeleniumTest.java
@@ -1,25 +1,12 @@
 package pom.test;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pom.page.HomePage;
-import waits.CustomConditions;
-
-import java.time.Duration;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.function.Function;
 
 public class WebDriverSeleniumTest {
     private WebDriver driver;
